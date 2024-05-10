40ae5fccc21ebb67f791d18f02def111161fdecd
hbja <hbja96@gmail.com>
2024-05-10 12:33:23 +0200

    Once more

--- pcse_gym/utils/defaults.py
+++ pcse_gym/utils/defaults.py
@@ -18,19 +18,10 @@ def get_wofost_default_po_features():
     return ["TAGP", "LAI", "NAVAIL", "NuptakeTotal", "SM"]
 
 
-
 def get_default_crop_features(pcse_env=0, minimal=True):
     if pcse_env and minimal:
         crop_features = get_wofost_minimal_crop_features()
     elif pcse_env:
-        crop_features = get_wofost_default_crop_features()
-    else:
-        crop_features = get_lintul_default_crop_features()
-    return crop_features
-
-
-def get_default_crop_features(pcse_env=1):
-    if pcse_env:
         crop_features = get_wofost_default_crop_features()
     else:
         crop_features = get_lintul_default_crop_features()
